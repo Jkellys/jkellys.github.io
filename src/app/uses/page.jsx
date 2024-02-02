0fd9cf6922087a4c7ce9abe2e717d941b16d47cc
jkellys <jonathankelly@hotmail.ca>
2024-02-02 11:25:36 -0800

    first commit

---
+++ src/app/uses/page.jsx
@@ -0,0 +1,79 @@
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Uses',
+  description: 'Software I use, gadgets I love, and other things I recommend.',
+}
+
+export default function Uses() {
+  return (
+    <SimpleLayout
+      title="Software I use, gadgets I love, and other things I recommend."
+      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+    >
+      <div className="space-y-20">
+        <ToolsSection title="Development tools">
+          <Tool title="Sublime Text 4">
+            I don’t care if it’s missing all of the fancy IDE features everyone
+            else relies on, Sublime Text is still the best text editor ever
+            made.
+          </Tool>
+          <Tool title="VS Code">
+            I use VS Code for all of my TypeScript and JavaScript work. It’s
+            great for debugging and has a ton of great extensions.
+          </Tool>
+          <Tool title="iTerm2">
+            I’m honestly not even sure what features I get with this that aren’t
+            just part of the macOS Terminal but it’s what I use.
+          </Tool>
+          <Tool title="TablePlus">
+            Great software for working with databases. Has saved me from
+            building about a thousand admin interfaces for my various projects
+            over the years.
+          </Tool>
+          <Tool title="Firebase">
+            I use Firebase for all of my personal projects. It’s great for
+            hosting, databases, and authentication.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Design">
+          <Tool title="Figma">
+            We started using Figma as just a design tool but now it’s become our
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Productivity">
+          <Tool title="Click Up">
+            We use Click Up for all of our project management. It’s a little
+            overwhelming at first but once you get the hang of it, it’s
+            incredibly powerful.
+          </Tool>
+        </ToolsSection>
+      </div>
+    </SimpleLayout>
+  )
+}
