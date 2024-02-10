5a762f95c458e99c0781a5d4a2b9776a2cf6dd90
jkellys <jonathankelly@hotmail.ca>
2024-02-10 13:26:24 -0800

    experment config

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,9 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
-module.exports = {
   experimental: {
-    images: true, // or any other valid configuration
+    images: true,
   },
-  // other configurations...
 }
 
 const withMDX = nextMDX({
