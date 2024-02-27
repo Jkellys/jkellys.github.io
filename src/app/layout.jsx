e657b1f9733a69ae06020644e28d868460b89075
Jkellys <jonathankelly@hotmail.ca>
2024-02-27 14:55:02 -0800

    avatar and tab test change

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,10 +6,9 @@ import '@/styles/tailwind.css'
 export const metadata = {
   title: {
     template: '%s - Jonathan Kelly',
-    default: 'Spencer Sharp - Software designer',
+    default: 'Jonathan Kelly - Software designer',
   },
-  description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+  description: 'I’m Jonathan Kelly, a software developer based in nanaimo BC.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
